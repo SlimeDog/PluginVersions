c494ea1addc0ebd6e965befd5b5d2e8e2eadfca0
Gabriel <vera.gabrielalejandro@gmail.com>
2021-11-20 13:07:43 -0400

    Update 1.0.7

--- src/main/java/com/straight8/rambeau/velocity/UpdateChecker.java
+++ src/main/java/com/straight8/rambeau/velocity/UpdateChecker.java
@@ -23,7 +23,7 @@ public class UpdateChecker {
 
     public UpdateChecker(PluginVersionsVelocity plugin, BiConsumer<VersionResponse, String> consumer) {
         this.plugin = plugin;
-        this.currentVersion = "1.0.6";
+        this.currentVersion = "1.0.7";
         this.versionResponse = consumer;
     }
 
