6df6af91ffbaa4be8c9e84a43ec85bef9d49a8cd
Mart drives_a_ford Ratas <mart.ratas@gmail.com>
2023-03-24 23:03:57 +0000

    Add spacing to velocity command

--- src/main/java/com/straight8/rambeau/velocity/PluginVersionsCmd.java
+++ src/main/java/com/straight8/rambeau/velocity/PluginVersionsCmd.java
@@ -1,5 +1,6 @@
 package com.straight8.rambeau.velocity;
 
+import com.straight8.rambeau.util.CommandPageUtils;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.plugin.PluginContainer;
@@ -8,8 +9,10 @@ import net.kyori.adventure.text.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class PluginVersionsCmd implements SimpleCommand {
+    private static final int LINES_PER_PAGE = 10;
 
     private final PluginVersionsVelocity plugin;
 
@@ -58,39 +61,52 @@ public class PluginVersionsCmd implements SimpleCommand {
             // Set page to 0 if illegal page was requested
             page = Math.max(page, 0);
 
-            int linesPerPage = 10;
             if (page > 0) {
-                if (((page - 1) * linesPerPage) < pluginList.size()) {
-                    sender.sendMessage(Component.text("PluginVersions ===== page " + page + " ====="));
+                if (((page - 1) * LINES_PER_PAGE) < pluginList.size()) {
+                    String msg = plugin.getConfig().getString("page-header-format",
+                            "PluginVersions ===== page {page} =====");
+                    sender.sendMessage(Component.text(msg.replace("{page}", String.valueOf(page))));
                 }
-                for (int i = ((page - 1) * linesPerPage); i < pluginList.size() && i < (page * linesPerPage); i++) {
+                int maxSpacing = CommandPageUtils.getMaxNameLength(plugin -> {
+                    Optional<String> name = plugin.getDescription().getName();
+                    return name.isPresent() ? name.get() : "N/A";
+                }, CommandPageUtils.getPage(pluginList, page, LINES_PER_PAGE));
+                for (int i = ((page - 1) * LINES_PER_PAGE); i < pluginList.size() && i < (page * LINES_PER_PAGE); i++) {
                     PluginContainer p = pluginList.get(i);
 
-                    String msg = plugin.getConfig().getString("enabled-version-format", " - &a{name} &e{version}");
+                    String msg = plugin.getConfig().getString("enabled-version-format", "&a{name}{spacing}&e{version}");
                     if(p.getDescription().getName().isPresent() && p.getDescription().getVersion().isPresent()) {
+                        String spacing = CommandPageUtils.getSpacingFor(p.getDescription().getName().get(), maxSpacing);
                         Component comp = Component
                                 .text(msg.replace("{name}", p.getDescription().getName().get()).replace("{version}",
-                                        p.getDescription().getVersion().get()));
+                                        p.getDescription().getVersion().get()).replace("{spacing}", spacing));
                         sender.sendMessage(comp);
                     } else if(p.getDescription().getId().equalsIgnoreCase("serverlistplus")) {
+                        String spacing = CommandPageUtils.getSpacingFor(p.getDescription().getName().get(), maxSpacing);
                         Component comp = Component
                                 .text(msg.replace("{name}", SLPUtils.getSLPName()).replace("{version}",
-                                        SLPUtils.getSLPVersion()));
+                                        SLPUtils.getSLPVersion()).replace("{spacing}", spacing));
                         sender.sendMessage(comp);
                     }
                 }
             } else {
+                int maxSpacing = CommandPageUtils.getMaxNameLength(plugin -> {
+                    Optional<String> name = plugin.getDescription().getName();
+                    return name.isPresent() ? name.get() : "N/A";
+                }, pluginList);
                 for (PluginContainer p : pluginList) {
-                    String msg = plugin.getConfig().getString("enabled-version-format", " - &a{name} &e{version}");
+                    String msg = plugin.getConfig().getString("enabled-version-format", "&a{name}{spacing}&e{version}");
                     if(p.getDescription().getName().isPresent() && p.getDescription().getVersion().isPresent()) {
+                        String spacing = CommandPageUtils.getSpacingFor(p.getDescription().getName().get(), maxSpacing);
                         Component comp = Component
                                 .text(msg.replace("{name}", p.getDescription().getName().get()).replace("{version}",
-                                        p.getDescription().getVersion().get()));
+                                        p.getDescription().getVersion().get()).replace("{spacing}", spacing));
                         sender.sendMessage(comp);
                     } else if(p.getDescription().getId().equalsIgnoreCase("serverlistplus")) {
+                        String spacing = CommandPageUtils.getSpacingFor(p.getDescription().getName().get(), maxSpacing);
                         Component comp = Component
-                                .text(msg.replace("{name}", SLPUtils.getSLPName()).replace("{version}",
-                                        SLPUtils.getSLPVersion()));
+                                .text(msg.replace("{name}", p.getDescription().getName().get()).replace("{version}",
+                                        p.getDescription().getVersion().get()).replace("{spacing}", spacing));
                         sender.sendMessage(comp);
                     }
                 }
